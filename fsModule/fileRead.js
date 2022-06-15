fd683c44dde74dc7ec1d871f13e4fc4488b911df
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-15 10:46:04 +0600

    Read a file(Asynchronous) example

---
+++ fsModule/fileRead.js
@@ -0,0 +1,17 @@
+var http=require('http');
+var fs=require('fs');
+
+var server=http.createServer(function (req,res) {
+
+    if(req.url=='/'){
+        fs.readFile('home.html',function (error,data) {
+           res.writeHead(200,{'content-type':'text/html'});
+           res.write(data);
+           res.end();
+        });
+    }
+
+});
+
+server.listen(3000);
+console.log('server start successfully');
